cd569f435d0fc2be38b7a0068bd0ebde670da237
emil817 <eldamar17@mail.ru>
2023-07-04 15:31:11 +0700

    04.07.2023

--- imdbSirius/moviedash/dash/movies_diagram.py
+++ imdbSirius/moviedash/dash/movies_diagram.py
@@ -1,14 +1,17 @@
-from dash import html, dcc
+from dash import html, dcc, no_update
 from django_plotly_dash import DjangoDash
 import plotly.graph_objects as go
 from movies.models import Movie, Director, Type, OriginalName, Duration
 import dash_bootstrap_components as dbc
-from dash.dependencies import Output, Input
+from dash.dependencies import Output, Input, State
 from django.db.models import Count
 
+
+layout_dropdown = [{'label':'Тип', 'value':'type'}, {'label':'Режисёры', 'value':'director'}, {'label':'Длительность', 'value':'duration'}]
 app = DjangoDash('MovieDiagram')
 app.layout = html.Div(
     [html.H1("Диаграмма режисёров"),
+    dcc.Dropdown(id='dropdown_diagram',options=layout_dropdown),
     dcc.Graph(id='movies_diagram'),
     dbc.Button(id='update_diagram', children='Обнови диаграмму', n_clicks=0)]
 )
@@ -16,14 +19,21 @@ app.layout = html.Div(
 
 @app.callback(
     Output('movies_diagram', 'figure'),
-    Input('update_diagram', 'n_clicks')
+    Input('update_diagram', 'n_clicks'),
+    Input('dropdown_diagram', 'value')
 )
-def update_movie_diagram(n):
-    directors_movies_count = Director.objects.all().values('name').annotate(movies_count=Count('movie'))
+def update_movie_diagram(n, value):
+    if value=='director':
+        directors_movies_count = Director.objects.all().values('name').annotate(movies_count=Count('movie'))
+    elif value=='type':
+        directors_movies_count = Type.objects.all().values('name').annotate(movies_count=Count('movie'))
+    else:
+        return no_update
     data = go.Pie(
         values=[director['movies_count'] for director in directors_movies_count],
         labels=[director['name'] for director in directors_movies_count]
-    )
+    )        
+
     layout = {
         'showlegend':True
     }
